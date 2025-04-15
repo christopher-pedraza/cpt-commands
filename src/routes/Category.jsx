c637e76edc1a14fbe1657e1244c83fb3977d0d3d
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-04-15 12:13:33 -0600

    Refactor: Update Tailwind CSS version and enhance Category route
    - Updated Tailwind CSS version in package.json to ^3.4.17.
    - Enhanced Category component to fetch and display section data from Redux store.
    - Removed JSON data dispatch from Home component and moved it to Root component for better data management.

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,3 +1,40 @@
+import { useSelector } from "react-redux";
+import { selectFileById } from "@/redux/Slices/jsonSlice";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { Card, CardBody, CardHeader, Button } from "@heroui/react";
+
 export default function Category() {
-    return <div></div>;
+    const { category_id } = useParams();
+    // Get the json data from the redux store
+    const sectionData = useSelector((state) =>
+        selectFileById(state, category_id)
+    );
+
+    // print the section data to the console
+    useEffect(() => {
+        console.log(sectionData);
+    }, [sectionData]);
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">{sectionData?.title}</h1>
+            <Button>Hola</Button>
+            <Card fullWidth>
+                <CardHeader className="text-xl font-bold">
+                    {sectionData?.title}
+                </CardHeader>
+                <CardBody className="flex flex-col">
+                    {sectionData?.sections.map((section, index) => (
+                        <div key={index} className="m-2">
+                            <h2 className="text-lg font-bold">
+                                {section.title}
+                            </h2>
+                            <p>{section.description}</p>
+                        </div>
+                    ))}
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,6 @@
 import LinksList from "@/components/LinksList";
-import { useDispatch } from "react-redux";
-import { setFiles } from "@/redux/Slices/jsonSlice";
 
 export default function Home() {
-    const dispatch = useDispatch();
-    // import all the json files from @jsons folder
-    const jsonFiles = import.meta.glob("@/jsons/*.json", { eager: true });
-    const jsonData = Object.values(jsonFiles).map((file) => file.default);
-
-    // dispatch the json data to the redux store
-    dispatch(setFiles(jsonData));
-
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <LinksList />
