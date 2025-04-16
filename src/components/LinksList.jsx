d2b717df3664980f0fb80960eb32dd34f8d379de
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-04-16 00:05:21 -0600

    Create masonry link list

--- src/components/LinksList.jsx
+++ src/components/LinksList.jsx
@@ -1,32 +1,37 @@
 import { Link } from "@heroui/link";
-
 import { useSelector } from "react-redux";
 import { selectFiles } from "@/redux/Slices/jsonSlice";
-
-import { Card, CardBody, CardHeader, ScrollShadow } from "@heroui/react";
+import { Card, CardBody, CardHeader } from "@heroui/react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 export default function LinksList() {
     // Get the json data from the redux store
     const jsonData = useSelector(selectFiles);
 
     return (
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 w-4/5 h-auto">
-            {jsonData.map((data, index) => (
-                <Card key={index} className="self-start">
-                    <CardHeader>
-                        <Link href={`${data.id}`}>{data.title}</Link>
-                    </CardHeader>
-                    <CardBody>
-                        {data.sections.map((section, index) => (
-                            <div key={index} className="ml-4">
-                                <Link href={`${data.id}#${section.id}`}>
-                                    {section.title}
-                                </Link>
-                            </div>
-                        ))}
-                    </CardBody>
-                </Card>
-            ))}
+        <div className="w-4/5 h-auto">
+            <ResponsiveMasonry
+                columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+            >
+                <Masonry gutter="16px">
+                    {jsonData.map((data, index) => (
+                        <Card key={index} className="m-4" fullWidth>
+                            <CardHeader>
+                                <Link href={`${data.id}`}>{data.title}</Link>
+                            </CardHeader>
+                            <CardBody>
+                                {data.sections.map((section, index) => (
+                                    <div key={index} className="ml-4">
+                                        <Link href={`${data.id}#${section.id}`}>
+                                            {section.title}
+                                        </Link>
+                                    </div>
+                                ))}
+                            </CardBody>
+                        </Card>
+                    ))}
+                </Masonry>
+            </ResponsiveMasonry>
         </div>
     );
 }
